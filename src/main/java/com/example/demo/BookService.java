4359bb2d60ba44f07363b74e6c74ed3129462689
Atakan <atakankahil2811@gmail.com>
2024-08-04 20:57:38 +0200

    initial commit

---
+++ src/main/java/com/example/demo/BookService.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BookService {
+    private final BookRepository bookRepository;
+
+    @Autowired
+    public BookService(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public List<Book> getAllBooks() {
+        return bookRepository.findAll();
+    }
+
+    public Book saveBook(Book book) {
+        return bookRepository.save(book);
+    }
+}
---
+++ src/main/java/com/example/demo/DataInitializer.java
@@ -0,0 +1,23 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    private final BookService bookService;
+
+    @Autowired
+    public DataInitializer(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        bookService.saveBook(new Book("The Catcher in the Rye", "J.D. Salinger"));
+        bookService.saveBook(new Book("To Kill a Mockingbird", "Harper Lee"));
+        bookService.saveBook(new Book("1984", "George Orwell"));
+    }
+}
